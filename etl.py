5dc13c5de5c30cb3b0a4ad188fa97fe19a8c56fb
Pratyush <lc23-0429@lclark.edu>
2026-02-25 13:20:17 -0800

    added basic functions

--- etl.py
+++ etl.py
@@ -1,6 +1,14 @@
 import csv
 
 def read_data():
-        with open('junction.csv', mode='r', encoding='utf-8') as csv_file:
-            reader = csv.reader(csv_file)
-            next(reader) # Skip header
+    Tree = []
+    Bird = []
+    with open('junction.csv', mode='r', encoding='utf-8') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for row in reader:
+            Bird.append(row[0])
+            Tree.append(row[1])
+        
+
+read_data()
