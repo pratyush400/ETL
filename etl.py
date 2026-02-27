c5cb2210e1fda4fbc721092d6c0c5dba039dbc2c
Pratyush <lc23-0429@lclark.edu>
2026-02-26 21:17:33 -0800

    made a table method

--- etl.py
+++ etl.py
@@ -5,51 +5,68 @@ class ETL:
         self.reader = []
         self.treeID = []
         self.birdID = []
+        self.tree_NestsIn = []
+        self.bird_NestsIn = []
+        self.year_NestsIn = []
+        self.query_NestsIn = []
+        self.bird_SpeciesC = []
+        self.bird_SpeciesS = []
+        self.bird_SpeciesID = []
+
 
     def read_data(self):
-        i = 1
+        i_birdID = 1
+        i_birdSciID = 1
+    
         with open('tree.csv', mode='r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
             next(reader)
             for row in reader:
                 self.treeID.append(row[2])
 
-        with open('junction.csv', mode='r', encoding='utf-8') as csv_file:
+        with open('bird.csv', mode='r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
             next(reader)
-            for row in enumerate(reader):
-                self.birdID.append(i)
-                i = i + 1
-        
-        # print(self.birdID)
-        print(self.treeID)
+            for row in reader:
+                self.bird_SpeciesC.append(row[1])
+                self.bird_SpeciesS.append(row[2])
+                self.bird_SpeciesID.append(i_birdSciID)
+                i_birdSciID += 1
+        # print(self.bird_SpeciesID)
 
-
-    def read_data_NestsIn():
-        Tree_NestsIn = []
-        Bird_NestsIn = []
-        Year_NestsIn = []
-        Query_NestsIn = []
         with open('junction.csv', mode='r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
             next(reader)
             for row in reader:
-                Bird_NestsIn.append(row[0])
-                Tree_NestsIn.append(row[1])
-                Year_NestsIn.append(row[2])
-        # print(Year)
+                self.bird_NestsIn.append(row[0])
+                self.year_NestsIn.append(row[2])
+                self.tree_NestsIn.append(row[1])
+
+            for row in range(len(self.bird_NestsIn)):
+                self.birdID.append(i_birdID)
+                i_birdID = i_birdID + 1
+
+    def create_table(self):
+        birdSpecTable = []
+
+        for i in range(len(self.bird_SpeciesID)):
+            temp = f'''INSERT INTO BirdSpecies (ID, SpeciesCommon, SpeciesScientific ){os.linesep}VALUES ({self.bird_SpeciesID[i]},'{self.bird_SpeciesC[i]}','{self.bird_SpeciesS[i]}');{os.linesep}{os.linesep}'''
+            birdSpecTable.append(temp)
+
+        # print(birdSpecTable)
+
+        with open('birdSpecTable.sql', 'w', encoding='utf-8') as f:
+            for item in birdSpecTable:
+                f.write(item)
+
+        
 
 
 
-            for i in range(len(Tree_NestsIn)):
-                temp = f'''INSERT INTO NestsIn (TreeID, BirdID, NestYear ){os.linesep}VALUES ({Tree_NestsIn[i]},{Bird_NestsIn[i]},{Year_NestsIn[i]}){os.linesep}{os.linesep}'''
-                Query_NestsIn.append(temp)
-
-            for i in range(len(Tree_NestsIn)):
-                print(Query_NestsIn[i])
 
 if __name__ == '__main__':
     run = ETL()
     run.read_data()
+    run.create_table()
 
 
